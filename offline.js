1727a9a6eb3d481abd18126e25946e547c3387f0
Rafael Gonçalves Martins <128759336+DungeonCodes@users.noreply.github.com>
2025-05-29 14:11:59 -0300

    html5

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1748538698,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundtile.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/solidtile.png",
+		"images/jumpthrutile.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
